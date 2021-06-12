effbfe4f00fc9e81b71088177a62c14689fcec5e
gzhegow <6562680@gmail.com>
2021-06-13 02:48:40 +0300

    m

--- src/Domain/Str/SluggerInterface.php
+++ src/Domain/Str/SluggerInterface.php
@@ -9,44 +9,6 @@ namespace Gzhegow\Support\Domain\Str;
 interface SluggerInterface
 {
     /**
-     * @return null|string
-     */
-    public function getDefaultLocale() : ?string;
-
-    /**
-     * @return array
-     */
-    public function getSequencesMap() : array;
-
-    /**
-     * @return array
-     */
-    public function getSymbolsMap() : array;
-
-
-    /**
-     * @param array|\Closure $defaultLocale
-     *
-     * @return static
-     */
-    public function defaultLocale($defaultLocale);
-
-    /**
-     * @param array|\Closure $sequencesMap
-     *
-     * @return static
-     */
-    public function sequencesMap($sequencesMap);
-
-    /**
-     * @param array|\Closure $symbolsMap
-     *
-     * @return static
-     */
-    public function symbolsMap($symbolsMap);
-
-
-    /**
      * @param string      $string
      * @param null|string $delimiter
      * @param null|string $locale
